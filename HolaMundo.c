b79e30e66c21ec295e3f070487c74d8f80f26384
Ana Paula Percino Huerta <128005733+pauph@users.noreply.github.com>
2023-08-10 12:26:43 -0600

    Add files via upload

---
+++ HolaMundo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main (int argc,char **argv)
+{
+	
+	printf("Primera generación (1940-1956): Tubos al vacio \n");
+	printf("*Utilización de válvulas de vacío para procesamiento. \n");
+	printf("*Tamaño y peso enormes, ocupaban grandes salas. \n");
+	printf("*Programación en lenguaje de máquina y cableado manual. \n\n");
+	
+	printf("Segunda generación (1956-1963): Transistores\n");
+	printf("*Transistores reemplazaron las válvulas de vacío. \n");
+	printf("*Uso de cintas magnéticas para almacenamiento. \n");
+	printf("*Surgimiento de sistemas operativos y acceso compartido. \n\n");
+	
+	printf("Tercera Generación (1964-1971): \n");
+	printf("*Introducción de circuitos integrados (chips). \n");
+	printf("*Implementación de sistemas de tiempo compartido. \n");
+	printf("*Desarrollo de sistemas operativos avanzados. \n\n");
+	
+	printf("Cuarta Generación (1971-1988): \n");
+	printf("*Invención de microprocesadores y chips. \n");
+	printf("*Popularización de las computadoras personales (PC). \n");
+	printf("*Introducción de interfaces gráficas de usuario (GUI). \n\n");
+	
+	printf("Quinta Generación (1991-actualidad): \n");
+	printf("*Avances en tecnología VLSI para chips más pequeños y potentes.\n");
+	printf("*Investigación en inteligencia artificial (IA) y aprendizaje automático. \n");
+	printf("*Desarrollo de redes globales y expansión de Internet. \n\n");
+	
+	return 0;
+}
